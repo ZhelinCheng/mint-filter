97683f592aeae77937106aea60ed8befe40717b8
ZheLin <chengzhelin@foxmail.com>
2019-06-08 18:06:47 +0800

    修改说明

--- src/index.ts
+++ src/index.ts
@@ -146,7 +146,8 @@ export = Mint
 if (require.main === module) {
   // ['bd', 'b'] 1bbd2 1bdb2 1bbdb2
   // ['bd', 'db'] 1bddb2
-  let m = new Mint(['淘宝', '拼多多', '京东'])
+  let m = new Mint(['淘宝', '拼多多', '京东', 'TEST'])
   console.log(m.filterSync('双十一在淘宝买东西，618在京东买东西，当然你也可以在拼多多买东西。'))
+  console.log(m.filterSync('这是另外的TEST字符串'))
   console.log(m.everySync('测试这条语句是否能通过，加上任意一个关键词京东'))
 }
