cdd73244d08ec14e06ceba621de0ed20b563e743
ZhelinCheng <i@zhelin.me>
2019-11-28 11:39:28 +0800

    删除冗余

--- src/index.ts
+++ src/index.ts
@@ -153,10 +153,10 @@ class Mint extends Tree {
 
 export = Mint
 
-if (require.main === module) {
+/* if (require.main === module) {
   let m = new Mint(['拼多多', '淘宝', '京东', 'TEST', 'aaaa', 12345])
   console.log(m.filterSync('双十一在淘宝买东西，618在京东买东西，当然你也可以在拼多多买东西。'))
   console.log(m.filterSync('这是另外的TEST字符串，aaaa也是敏感词，123456中也有敏感词'))
   console.log(m.everySync('测试这条语句是否能通过，加上任意一个关键词京东'))
   console.log(m.includes('测试这条语句是否能通过，加上任意一个关键词京东'))
-}
+} */
