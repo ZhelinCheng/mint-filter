bcac4335f104c5f02d9d961cc92f3b47da2afffc
ZhelinCheng <i@zhelin.me>
2019-11-29 15:29:03 +0800

    优化匹配方法

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ class Mint extends Tree {
     this.createFailureTable()
   }
 
-  private searchKey () {
+  private searchKey() {
 
   }
 
@@ -52,57 +52,65 @@ class Mint extends Tree {
 
     // 保存过滤文本
     let filterText: string = ''
+    let keywords: string = ''
 
     // 是否通过，无敏感词
     let isPass = true
 
     // 下一个Node与当前Node
     let searchNode: Node = this.root
-    let currNode: Node | boolean
+    // let currNode: Node | boolean
 
-    /* for (endIndex; endIndex <= wordLen; endIndex++) {
-      let key: string = word[endIndex]
-      currNode = this.search(key, prevNode.children)
-    } */
-
-    // 开始匹配
+    // 是否开始匹配
     let isStart = false
+
 
     while (endIndex < wordLen) {
       let key: string = word[endIndex]
-      currNode = this.search(key, searchNode.children)
+      let currNode: Node | boolean = this.search(key, searchNode.children)
+      filterText += key
       // 判断是否找到
       if (currNode) {
         if (!isStart) {
           startIndex = endIndex
           isStart = true
+          keywords = ''
         }
 
+        // 是否匹配成功
         if (isStart && currNode.word) {
-          console.log(startIndex, endIndex, key)
-          isStart = false
+          isStart = isPass = false
+          keywords += key
+          console.log(startIndex, endIndex, key, filterText)
+          // console.log('*'.repeat(keywords.length))
+          filterKeywords.push(keywords)
         }
-      } else {
-        // 如果没有匹配到
+      } else if (isStart) {
+        // 如果没有匹配到，走失配流程
         currNode = searchNode.failure
+        key = currNode.key
+        isStart = false
+        keywords = ''
 
-        if (currNode) {
-          const { key, word } = currNode
-          isStart = false
-          if (key !== 'root') {
-            startIndex = endIndex - 1
-            isStart = true
-          }
+        if (key !== 'root') {
+          startIndex = endIndex - 1
+          isStart = true
+        }
 
-          if (word) {
-            console.log(startIndex, endIndex)
-            isStart = false
-          }
+        // 是否匹配成功
+        if (currNode.word) {
+          console.log(startIndex, endIndex - 1, key, filterText)
+          isStart = isPass = false
+          keywords += key
+          // console.log('*'.repeat(keywords.length))
+          filterKeywords.push(keywords)
+          endIndex -= 2
+          // filterText = filterText.substr(0, startIndex) + '*'.repeat(keywords.length)
         }
       }
 
+      if (isStart) keywords += key
       searchNode = currNode || this.root
-      // searchNode = currNode ? currNode : (searchNode.failure || this.root)
       endIndex++
     }
 
@@ -159,8 +167,8 @@ export = Mint
 if (require.main === module) {
   /* let m = new Mint(['的', '我的天'])
   console.log(m.filterSync('开我的地，哈哈哈我的天')) */
-  let m = new Mint(['拼多多', '淘宝', '京东', 'TEST', 'aaaa', 12345, '大龄哥吃饭', '大龄哥'])
-  console.log(m.filterSync('双十一在买东西，618在京东买东西，当然你也可以在拼多多买东西。大龄哥买东西，大龄哥吃饭'))
+  let m = new Mint(['我的天', '的', '大龄哥', '大龄哥你好', 33])
+  console.log(m.filterSync('我的地啊，我的天啊，我的大龄哥啊，大龄哥你好啊33。'))
   // console.log(m.filterSync('这是另外的TEST字符串，aaaa也是敏感词，123456中也有敏感词'))
   // console.log(m.everySync('测试这条语句是否能通过，加上任意一个关键词京东'))
   // console.log(m.includes('测试这条语句是否能通过，加上任意一个关键词京东')
